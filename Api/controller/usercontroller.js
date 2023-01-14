645a4ee3cc62759b7204f28c0e3838bb708a3812
Antonio <antonioestebanreyes8@gmail.com>
2023-01-13 21:29:07 -0600

    feat: CRUD_read

--- Api/controller/usercontroller.js
+++ Api/controller/usercontroller.js
@@ -11,6 +11,19 @@ const create = async (req,res)=>{
         })
     }
 }
+const read=async (req,res)=>{
+    try {
+        const User= user.find(req.query)
+        return res.json({
+            msg:'usurio encontrado',User
+        })
+    } catch (error) {
+        return res.status(500).json({
+            msg:'Error al buscar al usuario',error
+        })
+        
+    }
+}
 export {
     create
 }
